6eb3565a5b881e75ef83308bfacb25e7a31f5f2b
liyp <liyp@its-cn.com>
2018-09-27 17:00:36 +0800

    登录添加了验证（账号是否存在，密码是否正确）；

--- node/tools/Tools.js
+++ node/tools/Tools.js
@@ -1,39 +1,47 @@
 const md5 = require('md5');
 class Tools {
-	constructor(){
-		this.sort = 'yangcz'
-	}
-	
-	isPoneAvailable(pone) {
-		let myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
-		if (!myreg.test(pone)) {
-		  return false;
-		} else {
-		  return true;
-		}
-	}
-  
-	md5(str){
-		return md5(str);
-	}
-	
-	userToken(username){
-		return this.md5(username+this.sort);
-	}
-	successRet(code,msg,data = {}){
-		return {'code':code,'msg':msg,'success':true,'data':data};
-	}
-	failRet(code,msg){
-		return {'code':code,'msg':msg,'success':false};
-	}
-	post(req){
-		let ret = {};
-		for(let index in req.body){
-			if(index === 'page') ret.page = parseInt(req.body[index]);
-			if(index === 'pageSize') ret.pageSize = parseInt(req.body[index]);
-			ret[index] = req.body[index];
-		}
-		return ret;
-	}
+  constructor() {
+    this.sort = 'yangcz'
+  }
+
+  isPoneAvailable(pone) {
+    let myreg = /^[1][3,4,5,7,8][0-9]{9}$/;
+    if (!myreg.test(pone)) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  md5(str) {
+    return md5(str);
+  }
+
+  userToken(username) {
+    return this.md5(username + this.sort);
+  }
+  successRet(code, msg, data = {}) {
+    return { 'code': code, 'msg': msg, 'success': true, 'data': data };
+  }
+  failRet(code, msg) {
+    return { 'code': code, 'msg': msg, 'success': false };
+  }
+  post(req) {
+    let ret = {};
+    for (let index in req.body) {
+      if (index === 'page') ret.page = parseInt(req.body[index]);
+      if (index === 'pageSize') ret.pageSize = parseInt(req.body[index]);
+      ret[index] = req.body[index];
+    }
+    return ret;
+  }
+  //判断data值是否有效
+  isValid(data) {
+    if (typeof (data) != "undefined" && data != null && data != 'null' && !(data === '')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
 module.exports = new Tools();
--- vue/src/view/user/login.vue
+++ vue/src/view/user/login.vue
@@ -1,93 +1,102 @@
 <template>
-    <body class="signin">
+
+  <body class="signin">
     <div class="signinpanel">
-        <div class="row">
-            <div class="col-sm-7">
-                <div class="signin-info">
-                    <div class="logopanel m-b">
-                        <h1>[ H+ ]</h1>
-                    </div>
-                    <div class="m-b"></div>
-                    <h4>欢迎使用 <strong>H+ 后台主题UI框架</strong></h4>
-                    <ul class="m-b">
-                        <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势一</li>
-                        <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势二</li>
-                        <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势三</li>
-                        <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势四</li>
-                        <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势五</li>
-                    </ul>
-                    <strong>还没有账号？ <a href="#">立即注册&raquo;</a></strong>
-                </div>
+      <div class="row">
+        <div class="col-sm-7">
+          <div class="signin-info">
+            <div class="logopanel m-b">
+              <h1>[ H+ ]</h1>
             </div>
-            <div class="col-sm-5">
-                <form>
-                    <h4 class="no-margins">登录：</h4>
-                    <p class="m-t-md">登录到H+后台主题UI框架</p>
-                    <input type="text" class="form-control uname" v-model="username"   placeholder="用户名/手机号" />
-                    <input type="password" class="form-control pword m-b" v-model="password"  placeholder="密码" />
-                    <!--<a href="">忘记密码了？</a>-->
-                    <a class="btn btn-success btn-block" @click="login">登录</a>
-                </form>
-            </div>
+            <div class="m-b"></div>
+            <h4>欢迎使用 <strong>H+ 后台主题UI框架</strong></h4>
+            <ul class="m-b">
+              <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势一</li>
+              <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势二</li>
+              <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势三</li>
+              <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势四</li>
+              <li><i class="fa fa-arrow-circle-o-right m-r-xs"></i> 优势五</li>
+            </ul>
+            <strong>还没有账号？ <a href="#">立即注册&raquo;</a></strong>
+          </div>
         </div>
-        <div class="signup-footer">
-            <div class="pull-left">
-                &copy; 2018 All Rights Reserved.
-            </div>
+        <div class="col-sm-5">
+          <form>
+            <h4 class="no-margins">登录：</h4>
+            <p class="m-t-md">登录到H+后台主题UI框架</p>
+            <input type="text" class="form-control uname" v-model="username"   placeholder="用户名/手机号" />
+            <input type="password" class="form-control pword m-b" v-model="password"  placeholder="密码" />
+            <!--<a href="">忘记密码了？</a>-->
+            <a class="btn btn-success btn-block" @click="login">登录</a>
+          </form>
         </div>
+      </div>
+      <div class="signup-footer">
+        <div class="pull-left">
+          &copy; 2018 All Rights Reserved.
+        </div>
+      </div>
     </div>
-    </body>
+  </body>
 </template>
 <script>
-    import '../../../static/css/login.css';
-    export default {
-        data(){
-            return {
-                username:'',
-                password:''
-            }
-        },
-        mounted:function(){
-
-        },
-        methods:{
-            login(){
-                if(!this.username){
-                    this.$message({
-                        message: '用户名或手机号不能空！',
-                        type: 'warning',
-                        showClose: true,
-                        center: true,
-                        duration:3000
-                    });
-                    return false;
-                }
-                if(!this.password){
-                    this.$message({
-                        message: '密码不能为空！',
-                        type: 'warning',
-                        showClose: true,
-                        center: true,
-                        duration:3000
-                    });
-                    return false;
-                }
-                this.Http.post('/user/login',{
-                    username:this.username,
-                    password:this.password
-                }).then(res=>{
-                    if(res.success){
-                        this.Storage.set('username',res.data.username);
-                        this.Storage.set('token',res.data.token);
-                        this.Common.writeRouter(res.data.juris,this.$router);
-                        this.$router.addRoutes(this.$router.options.routes);
-                        this.$router.push({path:'/index'});
-                    }
-                }).catch(error=>{
-                    this.$router.push({path:'/error'});
-                   console.error(error);
-                });
-                /*this.$http.post(
+import "../../../static/css/login.css";
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  },
+  mounted: function() {},
+  methods: {
+    login() {
+      if (!this.username) {
+        this.$message({
+          message: "用户名或手机号不能空！",
+          type: "warning",
+          showClose: true,
+          center: true,
+          duration: 3000
+        });
+        return false;
+      }
+      if (!this.password) {
+        this.$message({
+          message: "密码不能为空！",
+          type: "warning",
+          showClose: true,
+          center: true,
+          duration: 3000
+        });
+        return false;
+      }
+      this.Http.post("/user/login", {
+        username: this.username,
+        password: this.password
+      })
+        .then(res => {
+          if (res.success) {
+            this.Storage.set("username", res.data.username);
+            this.Storage.set("token", res.data.token);
+            this.Common.writeRouter(res.data.juris, this.$router);
+            this.$router.addRoutes(this.$router.options.routes);
+            this.$router.push({ path: "/index" });
+          } else {
+            this.$message({
+              message: res.msg,
+              type: "error",
+              showClose: true,
+              center: true,
+              duration: 3000
+            });
+          }
+        })
+        .catch(error => {
+          this.$router.push({ path: "/error" });
+          console.error(error);
+        });
+      /*this.$http.post(
                         '/login',
                         {
                             username:this.username,
@@ -98,7 +107,7 @@
                 }).catch(err=>{
 
                 });*/
-            }
-        }
     }
+  }
+};
 </script>
